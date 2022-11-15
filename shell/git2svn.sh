0fda6a289e31e61699c2b41bb54894b84d1ed55e
jushou <igetmail@163.com>
2022-11-15 22:36:07 +0800

    git2svn脚本增加特殊字符文件名等号和分号(=;)的处理

--- shell/git2svn.sh
+++ shell/git2svn.sh
@@ -256,8 +256,8 @@ cp_file_rev()
 		r_dirname_org=`dirname $2`
 		r_dirname_new=`dirname $3`
 	else
-		r_dirname_org=`echo $2 | sed -e 's/ /\\ /g' -e 's/(/\\(/g' -e 's/)/\\)/g' -e 's/&/\\&/g' | sed 's#[^/]*$##g'`
-		r_dirname_new=`echo $3 | sed -e 's/ /\\ /g' -e 's/(/\\(/g' -e 's/)/\\)/g' -e 's/&/\\&/g' | sed 's#[^/]*$##g'`
+		r_dirname_org=`echo $2 | sed -e 's/ /\\ /g' -e 's/(/\\(/g' -e 's/)/\\)/g' -e 's/&/\\&/g' -e 's/=/\\=/g' -e 's/;/\\;/g' | sed 's#[^/]*$##g'`
+		r_dirname_new=`echo $3 | sed -e 's/ /\\ /g' -e 's/(/\\(/g' -e 's/)/\\)/g' -e 's/&/\\&/g' -e 's/=/\\=/g' -e 's/;/\\;/g' | sed 's#[^/]*$##g'`
 	fi
 	svn_a_cmd=""
 	svn_d_cmd=""
@@ -322,7 +322,7 @@ cp_file_rev()
 				mkdir -p `dirname $PATCH_DIR_MODIFIED/$r_file`
 				mv -f $temp_file $PATCH_DIR_MODIFIED/$r_file
 			else ##空格文件名特殊处理
-				r_file_full=`echo $PATCH_DIR_MODIFIED/$r_file | sed -e 's/ /\\ /g' -e 's/(/\\(/g' -e 's/)/\\)/g' -e 's/&/\\&/g' | sed 's#[^/]*$##g'`
+				r_file_full=`echo $PATCH_DIR_MODIFIED/$r_file | sed -e 's/ /\\ /g' -e 's/(/\\(/g' -e 's/)/\\)/g' -e 's/&/\\&/g' -e 's/=/\\=/g' -e 's/;/\\;/g' | sed 's#[^/]*$##g'`
 				echo "mkdir -p $r_file_full " > /tmp/tmp_git2svn.sh
 				echo "exit \$?" >> /tmp/tmp_git2svn.sh
 				bash /tmp/tmp_git2svn.sh
@@ -462,7 +462,7 @@ gen_patch()
 	#special_char=`awk '{{if(NF>4){print 1} else {print 0}}}' all_raw.diff`
 	special_char=`awk -F "\t" '{{if(NF>=3){print $2"___"$3} else {print $2}}}' all_raw.diff | grep " " | wc -l`
 	if [ $special_char -eq 0 ]; then
-		special_char=`grep "[()&]" all_raw.diff | wc -l`
+		special_char=`grep "[()&;=]" all_raw.diff | wc -l`
 	fi
 
 	if [ $special_char -eq 0 ]; then
@@ -509,8 +509,8 @@ gen_patch()
 			if [ `expr $a_size_i % 100` -eq 0 ]; then
 				echo -n "."
 			fi
-			file_old=`awk  -F "\t" 'NR=="'$a_size_i'" {if(NF>=2){print  $2} }' $PATCH_DIR/all_raw.diff | sed -e 's/ /\\\\ /g' -e 's/(/\\\\(/g' -e 's/)/\\\\)/g' -e 's/&/\\\\&/g'`
-			file_new=`awk  -F "\t" 'NR=="'$a_size_i'" {if(NF==3){print  $3} else {print $2}}' $PATCH_DIR/all_raw.diff | sed -e 's/ /\\\\ /g' -e 's/(/\\\\(/g' -e 's/)/\\\\)/g' -e 's/&/\\\\&/g'`
+			file_old=`awk  -F "\t" 'NR=="'$a_size_i'" {if(NF>=2){print  $2} }' $PATCH_DIR/all_raw.diff | sed -e 's/ /\\\\ /g' -e 's/(/\\\\(/g' -e 's/)/\\\\)/g' -e 's/&/\\\\&/g' -e 's/=/\\\\=/g' -e 's/;/\\\\;/g'`
+			file_new=`awk  -F "\t" 'NR=="'$a_size_i'" {if(NF==3){print  $3} else {print $2}}' $PATCH_DIR/all_raw.diff | sed -e 's/ /\\\\ /g' -e 's/(/\\\\(/g' -e 's/)/\\\\)/g' -e 's/&/\\\\&/g' -e 's/=/\\\\=/g' -e 's/;/\\\\;/g'`
 			file_mode=`awk 'NR=="'$a_size_i'" {print $2}' $PATCH_DIR/all_raw.diff`
 			file_action=`awk 'NR=="'$a_size_i'" {print $5}' $PATCH_DIR/all_raw.diff`
 			file_mode=${file_mode:1}
@@ -529,7 +529,7 @@ gen_patch()
 	mv $PATCH_DIR/temp_folder $PATCH_DIR/pending_folder
 
 	###特殊字符检测
-	special_char=`grep "[ ()&]" $PATCH_DIR/pending_folder | wc -l`
+	special_char=`grep "[ ()&=;]" $PATCH_DIR/pending_folder | wc -l`
 	space_folder_line=(`cat $PATCH_DIR/pending_folder | wc -l`)
 	###处理可能需要删除的文件夹
 	if [ $special_char -eq 0 ]; then
@@ -554,7 +554,7 @@ gen_patch()
 					echo "	while [ \"#\`ls -A \$tmp_ar\`\" == \"#\" ]" >> $PATCH_DIR/$SVN_CMD_FILE
 					echo "	do" >> $PATCH_DIR/$SVN_CMD_FILE
 					echo "		svn delete \$tmp_ar" >> $PATCH_DIR/$SVN_CMD_FILE
-					echo "		tmp_ar=\`echo \$tmp_ar | sed -e 's# #\\\\ #g' -e 's#(#\\\\(#g' -e 's#)#\\\\)#g' -e 's#&#\\\\&#g' | sed  -e 's#/+\$##g' -e 's#/*[^/]*\$##g'\`" >> $PATCH_DIR/$SVN_CMD_FILE
+					echo "		tmp_ar=\`echo \$tmp_ar | sed -e 's# #\\\\ #g' -e 's#(#\\\\(#g' -e 's#)#\\\\)#g' -e 's#&#\\\\&#g' -e 's#=#\\\\=#g' -e 's#;#\\\\;#g' | sed  -e 's#/+\$##g' -e 's#/*[^/]*\$##g'\`" >> $PATCH_DIR/$SVN_CMD_FILE
 					echo "	done" >> $PATCH_DIR/$SVN_CMD_FILE
 					echo "fi" >> $PATCH_DIR/$SVN_CMD_FILE
 				done
@@ -571,7 +571,7 @@ gen_patch()
 			echo "	while [ \"#\`ls -A \$tmp_ar\`\" == \"#\" ]" >> $PATCH_DIR/$SVN_CMD_FILE
 			echo "	do" >> $PATCH_DIR/$SVN_CMD_FILE
 			echo "		svn delete \$tmp_ar" >> $PATCH_DIR/$SVN_CMD_FILE
-			echo "		tmp_ar=\`echo \$tmp_ar | sed -e 's# #\\\\ #g' -e 's#(#\\\\(#g' -e 's#)#\\\\)#g' -e 's#&#\\\\&#g' | sed  -e 's#/+\$##g' -e 's#/*[^/]*\$##g'\`" >> $PATCH_DIR/$SVN_CMD_FILE
+			echo "		tmp_ar=\`echo \$tmp_ar | sed -e 's# #\\\\ #g' -e 's#(#\\\\(#g' -e 's#)#\\\\)#g' -e 's#&#\\\\&#g' -e 's#=#\\\\=#g' -e 's#;#\\\\;#g' | sed  -e 's#/+\$##g' -e 's#/*[^/]*\$##g'\`" >> $PATCH_DIR/$SVN_CMD_FILE
 			echo "	done" >> $PATCH_DIR/$SVN_CMD_FILE
 			echo "fi" >> $PATCH_DIR/$SVN_CMD_FILE
 		done
